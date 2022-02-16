63a23ac1747ed908f697d858bc26d4cf30ac7fcd
akiramehanbun <akiramehanbun@gmail.com>
2022-02-17 00:15:53 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Unchi!'));
+app.get('/', (req, res) => res.send('Hello chinchin!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
